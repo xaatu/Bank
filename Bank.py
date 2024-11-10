016e14c84ab53543fbe56ecd93c1e96ea4079cb8
xaatu <xaatu>
2024-11-10 12:58:53 +0000

    first commit

---
+++ Bank.py
@@ -0,0 +1,69 @@
+def show_balance(balance): 
+        print('********************')
+        print(f'Your balance is £{balance:.2f}')
+        print('********************')
+
+
+
+def deposit():
+    print('********************')
+    amount = float(input('Enter an amount to be deposited:'))
+    print('********************')
+    if amount < 0:
+        print('********************')
+        print('That is not a valid amount.')
+        print('********************')
+        return 0
+    else:
+        return amount
+    
+
+def withdraw(balance):
+    print('********************')
+    amount = float(input('Enter an amount to be withdrawn:'))
+    print('********************')
+    if amount < 0:
+        print('That is not a valid amount to withdraw.')
+        return 0
+    elif amount > balance:
+        print('Insufficient funds.')
+        return 0
+    else:
+        return amount
+        
+
+def main():
+    balance = 0
+    is_running = True
+
+
+    while is_running:
+        print('********************')
+        print("Banking Program")
+        print('********************')
+        print("1.Show Balance")
+        print("2.Deposit")
+        print("3.Withdraw")
+        print("4.Exit")
+        print('********************')
+        choice = input("Enter your choice (1-4): ")
+        
+
+        if choice == '1':
+            show_balance(balance)
+        elif choice == '2':
+            balance += deposit()
+        elif choice == '3':
+            balance -= withdraw(balance)
+        elif choice == '4':
+            is_running = False
+        else:
+            print('********************')
+            print('That is not a valid choice')
+            print('********************')
+    print('********************')
+    print('Thank you. Have a nice day!')
+    print('********************')
+
+if __name__ == '__main__':
+    main()
